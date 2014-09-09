0dc918a6d25c76fa3df0bb6117b83221c1ab9de7
Eli White <github@eli-white.com>
2014-09-09 23:55:59 +0200

    Loading the remote page and grabbing it's title

--- iframe.js
+++ iframe.js
@@ -3,7 +3,41 @@
 
 window.addEventListener("message", receiveMessage, false);
 
-function receiveMessage(event)
-{
-  console.log("got url", event.data.url);
+function receiveMessage(event) {
+  var url = event.data.url;
+
+  var ele = document.getElementById("url");
+
+  getPage(url).then(function(response) {
+    ele.textContent = response.title;
+  });
+}
+
+
+function getPage(url) {
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(request.response, "text/html");
+
+        resolve(doc);
+      } else {
+        // We reached our target server, but it returned an error
+        reject(request);
+      }
+    };
+
+    request.onerror = function(error) {
+      // There was a connection error of some sort
+      reject(error);
+    };
+
+    request.send();
+  });
 }
