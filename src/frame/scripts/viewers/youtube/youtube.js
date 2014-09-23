781a83c2a8fbf0ccf1f26ff544a2a8f54f7b5b10
Eli White <github@eli-white.com>
2014-09-23 16:47:56 +0300

    Cleaning up Youtube Viewer's template and making it match the style rules of the project.

--- src/frame/scripts/viewers/youtube/youtube.js
+++ src/frame/scripts/viewers/youtube/youtube.js
@@ -10,27 +10,29 @@ var YoutubeViewer = ViewerBase.extend({
 
     var id = Helpers.getQueryVariable(url, 'v');
 
-    var api = 'http://gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=jsonc';//-in-script&amp;callback=";
+    var api = 'http://gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=jsonc'; //-in-script&amp;callback=";
 
     Helpers.getJSON(api).then(function(json) {
-      /*jshint camelcase: false */
+      // jshint camelcase: false
+      // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
       var params = {
         thumb: json.data.thumbnail.hqDefault,
         uploader: json.data.uploader,
         category: json.data.category,
         title: json.data.title,
-        description: json.data.description.substring(0,250) + '...',
+        description: json.data.description.substring(0, 250) + '...',
         likeCount: json.data.likeCount,
         viewCount: json.data.viewCount,
         commentCount: json.data.commentCount,
 
         createdAt: Helpers.stringifyTimestamp(json.created_at_i)
       };
-      
+      // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+      // jshint camelcase: true
+
       self.applyTemplate(template(params));
     });
   }
-
 });
 
 module.exports = new YoutubeViewer();
